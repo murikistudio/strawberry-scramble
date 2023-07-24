cd31a72d979588fe1f7a12c5182a561b625f9de7
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-07-24 00:12:27 -0300

    feat: reorganizar assets (WIP)

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -3,4 +3,4 @@ extends Node
 
 # Event handlers
 func _on_Intro_intro_finished() -> void:
-	GameCore.change_scene_to("res://scenes/levels/test/test.tscn")
+	GameCore.change_scene_to("res://scenes/levels/test.tscn")
