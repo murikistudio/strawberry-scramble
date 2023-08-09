06f7ad991e367af6daeccea1bdf3a0db28d1fecc
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-08-09 14:08:37 -0300

    feat: adicionar addon virtual joystick

--- scenes/gui/joystick/joystick.gd
+++ scenes/gui/joystick/joystick.gd
@@ -4,12 +4,6 @@ extends MarginContainer
 # Variables
 export var enabled := false
 
-var _stick_left_position := Vector2.ZERO
-var _stick_left_pressing := false
-var _stick_left_axis_movement := Vector2.ZERO
-
-onready var _texture_rect_stick_left_back: TextureRect = find_node("TextureRectStickLeftBack")
-onready var _stick_left: Control = find_node("StickLeft")
 onready var _texture_button_jump: TextureButton = find_node("TextureButtonJump")
 onready var _texture_button_pause: TextureButton = find_node("TextureButtonPause")
 
@@ -29,28 +23,6 @@ func _process(_delta: float) -> void:
 	_center_pivot(_texture_button_jump)
 	_center_pivot(_texture_button_pause)
 
-	if _stick_left_pressing:
-		_stick_left.rect_position = _stick_left_position
-	else:
-		_stick_left.rect_position = _texture_rect_stick_left_back.rect_size / 2
-
-	if _stick_left_pressing:
-		_stick_left_axis_movement = _stick_left_position / _texture_rect_stick_left_back.rect_size
-		_stick_left_axis_movement.x = (clamp(_stick_left_axis_movement.x, 0.0, 1.0) - 0.5) * 2.0
-		_stick_left_axis_movement.y = (clamp(-(_stick_left_axis_movement.y - 1.0), 0.0, 1.0) - 0.5) * 2.0
-
-		if stepify(_stick_left_axis_movement.x, 0.1):
-			_update_input_action(
-				"move_left" if _stick_left_axis_movement.x < 0 else "move_right",
-				abs(_stick_left_axis_movement.x)
-			)
-
-		if stepify(_stick_left_axis_movement.y, 0.1):
-			_update_input_action(
-				"move_down" if _stick_left_axis_movement.y < 0 else "move_up",
-				abs(_stick_left_axis_movement.y)
-			)
-
 
 func _center_pivot(control: Control) -> void:
 	control.rect_pivot_offset = control.rect_size / 2
@@ -62,18 +34,6 @@ func _update_input_action(action: String, strength: float) -> void:
 
 	elif Input.is_action_pressed(action):
 		Input.action_release(action)
-
-
-func _on_TextureRectStickLeftBack_gui_input(event: InputEvent) -> void:
-	if event is InputEventScreenDrag:
-		_stick_left_position = event.position
-
-	if event is InputEventScreenTouch:
-		_stick_left_pressing = event.is_pressed()
-
-		if not event.is_pressed():
-			for input_name in ["move_left", "move_right", "move_up", "move_down"]:
-				_update_input_action(input_name, 0.0)
 
 
 func _on_TextureButtonJump_button_down() -> void:
