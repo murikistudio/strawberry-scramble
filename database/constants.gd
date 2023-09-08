cdfae2c4a4a0334f9b01b7af9c7bd7842616210b
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-09-08 00:15:27 -0300

    feat: criar database de constantes

---
+++ database/constants.gd
@@ -0,0 +1,8 @@
+extends Reference
+class_name DatabaseConstants
+# Definições de constantes gerais utilizadas no jogo.
+
+
+# Constants
+const COLOR_PENALTY := Color.darkgray
+const COLOR_COMPLETE := Color.lightgreen
