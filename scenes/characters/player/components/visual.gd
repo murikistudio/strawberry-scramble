0b350e8d1ccee36faaadcb4be5241a82f5597cd2
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2023-11-06 21:40:16 -0300

    fix: suavizar rotação do player

--- scenes/characters/player/components/visual.gd
+++ scenes/characters/player/components/visual.gd
@@ -2,8 +2,9 @@ extends PlayerBaseComponent
 
 
 # Variables
-export(Array, ShaderMaterial) var shaders_to_update: Array
 export(float, 0.5, 2.0, 0.01) var view_forward_multiplier := 1.5
+export(float, 1.0, 20.0, 0.1) var turn_speed := 10.0
+export(Array, ShaderMaterial) var shaders_to_update: Array
 onready var _mesh_direction: MeshInstance = player.find_node("MeshDirection")
 onready var _directional_light: DirectionalLight = player.find_node("DirectionalLight")
 
@@ -36,13 +37,17 @@ func _physics_process(delta: float) -> void:
 
 # Private methods
 # Atualiza as malhas visuais de acordo com o estado.
-func _process_visual(_delta: float) -> void:
-	var target_vec: Vector3 = player.global_translation + _get_axis_offset(player.move_weight) * view_forward_multiplier
-	var interp_vec: Vector3 = lerp(player.global_translation, target_vec, 0.01)
-
+func _process_visual(delta: float) -> void:
 	if player.move_weight.length() > 0.1:
-		player.look_at(interp_vec, Vector3.UP)
+		var target_rotation := lerp_angle(
+			player.global_rotation.y,
+			atan2(player.move_weight.x, -player.move_weight.y),
+			turn_speed * delta
+		)
 
+		player.global_rotation.y = target_rotation
+
+	var target_vec: Vector3 = player.global_translation + _get_axis_offset(player.move_weight) * view_forward_multiplier
 	_mesh_direction.global_translation = target_vec
 
 
