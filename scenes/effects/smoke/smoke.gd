c55b816ab82e0f2bff6d554a99afd6ebc6d2baa9
Joel Gomes da Silva <joelgomes1994@hotmail.com>
2024-03-13 11:22:07 -0300

    feat: desativar sombra na fumaça do jogador

--- scenes/effects/smoke/smoke.gd
+++ scenes/effects/smoke/smoke.gd
@@ -8,6 +8,7 @@ onready var _smoke: MeshInstance = $Visual/Smoke
 
 # Built-in overrides
 func _ready() -> void:
+	_smoke.cast_shadow = false
 	_smoke.mesh = _smoke.mesh.duplicate()
 	var tween := create_tween()
 	tween.set_trans(Tween.TRANS_QUAD)
